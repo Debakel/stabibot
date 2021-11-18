68ce4437a5240e71d6586412f76d29c34d3e0e64
Moritz Duchêne <2857237+Debakel@users.noreply.github.com>
2021-11-18 19:05:10 +0100

    Update telegram message

--- stabibot/main.py
+++ stabibot/main.py
@@ -1,12 +1,29 @@
+import locale
+from string import Template
 from typing import List
 
 from stabibot import telegram
 from stabibot.calendar import get_gaps
 from stabibot.intervals import Interval
 
+locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
+
+gap_template = Template('')
+
 
 def format_message(gaps: List[Interval]):
-    message = f"""Es fehlen noch folgende Schichten: {gaps}"""
+    x = [f"➡️ {gap.start_datetime.strftime('%A %H:%M')} bis {gap.end_datetime.strftime('%H:%M')}" for gap in gaps]
+    x = "\n".join(x)
+    message = f"""
+☝️
+Folgende Schichten sind noch nicht vergeben:
+   
+{x}
+
+Hier kannst du dich dafür eintragen: https://teamup.com/ks4o76civjv5vxqjr7
+
+LG Stabine ❤️
+    """
     return message
 
 
--- stabibot/settings.py
+++ stabibot/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 ICS_FEED = os.environ.get("ICS_FEED")
 TIME_ZONE = pytz.timezone("Europe/Berlin")
 RESOLUTION_MINUTES = 1
-DEFAULT_RANGE_HOURS = 24
+DEFAULT_RANGE_HOURS = 36
 MIN_PERSONS = 2
 
 TELEGRAM_AUTH_TOKEN = os.environ.get("TELEGRAM_AUTH_TOKEN")
