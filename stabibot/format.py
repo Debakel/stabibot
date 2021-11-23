86d82e6bb5eb251b31fda3832744610d85ab4b85
Moritz Duchêne <2857237+Debakel@users.noreply.github.com>
2021-11-23 22:54:52 +0100

    Update formatting of telegram messages
    Closes #2

--- stabibot/format.py
+++ stabibot/format.py
@@ -1,3 +1,19 @@
+"""Modul zum Formatieren der Telegramnachrichten.
+
+Beispielnachricht:
+
+    ☝️ Folgende Schichten sind noch nicht vergeben:
+
+    ➡️ Morgen 09:00 bis 09:45
+    ➡️ Morgen 10:00 bis 11:00
+    ➡️ Donnerstag 09:00 bis 10:45
+
+    Hier kannst du dich dafür eintragen: https://example.org/plan
+
+    LG Stabine ❤️
+"""
+
+from datetime import datetime
 from typing import List
 
 import pendulum
@@ -6,19 +22,38 @@ from stabibot import settings
 from stabibot.intervals import Interval
 
 
+def format_from_date(dt: datetime) -> str:
+    dt = pendulum.instance(dt)
+
+    if dt.date() == pendulum.today().date():
+        day = "Heute"
+    elif dt.date() == pendulum.tomorrow().date():
+        day = "Morgen"
+    else:
+        day = dt.format("dddd", locale="de")
+
+    hour = dt.format("HH:mm", locale="de")
+
+    return f"{day} {hour}"
+
+
+def format_to_date(dt: datetime) -> str:
+    dt = pendulum.instance(dt)
+    return dt.format("HH:mm")
+
+
 def format_message(gaps: List[Interval]) -> str:
     slots = []
     for gap in gaps:
-        start = pendulum.instance(gap.start_datetime)
-        end = pendulum.instance(gap.end_datetime)
-        msg = f"➡️ {start.format('dddd HH:mm', locale='de')} bis {end.format('HH:mm', locale='de')}"
+        start = format_from_date(gap.start_datetime)
+        end = format_to_date(gap.end_datetime)
+        msg = f"➡️ {start} bis {end}"
         slots.append(msg)
 
-    slots = '\n'.join(slots)
+    slots = "\n".join(slots)
 
     message = f"""
-☝️
-Folgende Schichten sind noch nicht vergeben:
+☝️ Folgende Schichten sind noch nicht vergeben:
    
 {slots}
 
